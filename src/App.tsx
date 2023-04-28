f9f0bfde787bfab3d7aa8fd581e59dc4e8708414
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-04-28 15:18:02 +0200

    todo: useContext for language selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom"
+
 import Home from "./pages/Home"
 import Registration from "./pages/Registration"
 
 
 function App() {
+
+  
 
   return (
     <BrowserRouter>
