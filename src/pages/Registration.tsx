9ee5d955afd5ee62e3d7ecba76a56369301b9f6f
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-05-02 15:50:43 +0200

    feat: adding more inputs to registration form

--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -28,16 +28,24 @@ const Registration: FC = () => {
     
   return (
     <main>
-        <input placeholder='Name of the Company' type="text" name="companyName" id="companyName" />
+        <input className='border-2' placeholder='Name of the Company' type="text" name="companyName" id="companyName" />
         <p>Company Headquarters</p>
-          <select name="countries" id="countries">
+          <select className='block border-2' name="countries" id="countries">
               {countries?.map((country) => {
                 return(
                     <option value={country.alpha3}>{country.name}</option>
                 )
               })}
           </select>
-        
+          <div className='block'>
+        <input className='border-2' type="text" name="city" id="city" placeholder='city'/>
+        <input className='border-2' type="number" name="zip-code" id="zip-code" placeholder='zip-code'/>  
+          </div>
+          <div>
+        <input className='border-2' type="text" name="street" id="street" placeholder='street'/>
+        <input className='border-2' type="text" name="streetAdress" id="streetAdress" placeholder='street adress'/>
+          </div>
+      <input className='border-2 w-32' type="number" min="1900" max="2099" step="1" name="foundingYear" id="foundingYear" placeholder='founding year'/>
     </main>
   )
 }
