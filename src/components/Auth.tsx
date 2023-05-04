115e9783fd43c6c36b6c99d73d658b5a56cb9b46
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-05-04 15:54:08 +0200

    fix: changed protected Route

---
+++ src/components/Auth.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import DashboardCompany from "../pages/DashboardCompany";
+
+const Auth = () => {
+
+    const [isLoading, setIsLoading] = useState(true);
+    const navigator = useNavigate();
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch("http://localhost:9999/api/v1/company-login_auth", { credentials: "include" });
+            if (response.ok) {
+                setIsLoading(false);
+                return;
+            }
+            
+        })()
+    }, [])
+
+    if (isLoading) {
+        return <h1>Loading...</h1>
+    }
+    else{
+        return(
+            <DashboardCompany />
+        )
+    }
+    
+}
+
+export default Auth;
