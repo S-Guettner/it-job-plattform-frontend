5ab56b6479b27d77897462e8f3dc4f9c3769a056
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-05-04 15:56:16 +0200

    fix: added page for login

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+import Login from '../components/Login.tsx'
+
+
+const LoginPage: FC = () => {
+  return (
+    <main>
+        <h1>login Page</h1>
+        <Login />
+    </main>
+  )
+}
+
+export default LoginPage
