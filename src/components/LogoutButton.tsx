411f5448e178048683c3eb71d5048946cf3f94ad
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-05-06 19:21:50 +0200

    fix: moved delete cookie logic to component  && added navigation

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,12 +1,39 @@
 import { FC } from 'react'
-
-
+import { useNavigate } from 'react-router-dom'
+import useUserStore from '../global/zustand.ts'
 
 const LogoutButton: FC = () => {
+
+    const userState = useUserStore()
+    console.log(userState.userLoggedInState)
+
+    const navigator = useNavigate()
+    const deleteJWT = async () => {
+        try {
+            const res = await fetch("http://localhost:9999/api/v1/company-logout", {
+                method: "POST",
+                credentials: "include",
+            })
+            if (res.ok) {
+                navigator("/company")
+            }
+
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    const logoutHandler = () => {
+        deleteJWT()
+        userState.userLoggedOut()
+    }
+
+
   
+    
     return (
         
-        <button className='border-2 p-2'>Logout</button>
+        <button onClick={logoutHandler} className='border-2 p-2'>Logout</button>
     
   )
 }
