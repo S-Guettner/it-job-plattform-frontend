f61ef0ea2a7162f8782e27e5a42af8104fdceb74
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-05-06 16:36:43 +0200

    feat: added LogoutButton component

---
+++ src/components/LogoutButton.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+
+
+
+const LogoutButton: FC = () => {
+  
+    return (
+        
+        <button className='border-2 p-2'>Logout</button>
+    
+  )
+}
+
+export default LogoutButton
