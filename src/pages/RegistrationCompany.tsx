8d1a9ea797de135131c13e9f1875af4035e24a28
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-05-06 20:46:45 +0200

    feat: added error messages for registration form

--- src/pages/RegistrationCompany.tsx
+++ src/pages/RegistrationCompany.tsx
@@ -1,4 +1,4 @@
-import { FC,useState, useEffect} from 'react'
+import { FC,useState, useRef} from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import countriesListGerman from '../data/countriesGerman.json'
 import countriesListEnglish from '../data/countriesEnglish.json'
@@ -17,37 +17,60 @@ interface country {
 
 const Registration: FC = () => {
 
-  const [language] = useState("de")
+  const passwordValidationMessage = () => {
+    if (password === undefined || password.length < 9){
+      setPasswordErrorMessage("Passwort muss mindestens 8 Zeichen lang sein")
+    } 
+    else if (!/[A-Z]/.test(password)) {
+      setPasswordErrorMessage("Passwort muss mindestens einen Großbuchstaben enthalten")
+    }
+    else if (!/[$&+,:;=?@#|'<>.^*()%!-]/.test(password)) {
+      setPasswordErrorMessage("Passwort muss mindestens ein Sonderzeichen enthalten")
+    }
+    else{
+      setPasswordErrorMessage("")
+    }
+  }
 
-  const [countries, setCountries] = useState<country[]>(countriesListGerman)
+  const [countries] = useState<country[]>(countriesListGerman)
 
-  // set country to choosen lang from context
-  useEffect(() => {
-    console.log(language)
-    setCountries(language === "de" ? countriesListGerman :  countriesListEnglish )
-  }, [language])
+  const [password,setPassword] = useState<string>()
+  const [confirmPassword,setConfirmPassword] = useState<string>()
+  const [passwordErrorMessage,setPasswordErrorMessage] = useState("")
+  
+  const firstNameRef = useRef<HTMLInputElement>(null)
+  const lastNameRef = useRef<HTMLInputElement>(null)
+  const companyNameRef = useRef<HTMLInputElement>(null)
+  const telephoneNumberRef = useRef<HTMLInputElement>(null)
+  const cityRef = useRef<HTMLInputElement>(null)
+  const zipCodeRef = useRef<HTMLInputElement>(null)
+  const streetRef = useRef<HTMLInputElement>(null)
+  const streetAdressRef = useRef<HTMLInputElement>(null)
+  const emailRef = useRef<HTMLInputElement>(null)
 
-
-    
+  const [countryState,setCountryState] = useState("Deutschland")
+  
+  console.log(countryState)
+  
   return (
     <main className='flex justify-center'>
       <section>
       <h1 className='text-2xl mb-10'>Registriere dein Unternehmen</h1>
-      <div className=''>
-        <input className='border-2 mb-2 pl-2' type="text" name="firstName" id="firstName" placeholder='Vorname'/>
-        <input className='border-2 mb-2 pl-2' type="text" name="lastName" id="lastName" placeholder='Nachname'/>
+      <div>
+          <input ref={firstNameRef} className='border-2 mb-2 pl-2' type="text" name="firstName" id="firstName" placeholder='Vorname'/>
+          <input ref={lastNameRef} className='border-2 mb-2 pl-2' type="text" name="lastName" id="lastName" placeholder='Nachname'/>
       </div>
       <div>
-        <input className='border-2 pl-2 ' type="text" name="companyName" id="companyName" placeholder='Unternehmensname'/>
-        <input className='border-2 pl-2  mb-5' type="tel" name="telephoneNumber" id="telephoneNumber" placeholder='Telefonnummer' pattern='[0-9]*"'/>
+          <input ref={companyNameRef} className='border-2 pl-2 ' type="text" name="companyName" id="companyName" placeholder='Unternehmensname'/>
+          <input ref={telephoneNumberRef} className='border-2 pl-2  mb-5' type="tel" name="telephoneNumber" id="telephoneNumber" placeholder='Telefonnummer' pattern='[0-9]*"'/>
       </div>
       <div>
-          <select className='mb-2 block border-2 w-full' name="countries" id="countries">
-              {countries?.map((country) => {
+          <select value={countryState} onChange={(e) => setCountryState(e.target.value)} className='mb-2 block border-2 w-full' name="countries" id="countries">
+            {countries?.map((country) => {
                 return(
                     <option 
                     key={uuidv4()}
-                    value={country.alpha3}>
+                    value={country.name}>
                       {country.name}
                       </option>
                 )
@@ -55,17 +78,20 @@ const Registration: FC = () => {
           </select>
       </div>
       <div className='block mb-2'>
-        <input className='border-2 pl-2' type="text" name="city" id="city" placeholder='Stadt'/>
-        <input className='border-2 pl-2' type="number" name="zip-code" id="zip-code" placeholder='Postleitzahl'/>  
+          <input ref={cityRef} className='border-2 pl-2' type="text" name="city" id="city" placeholder='Stadt'/>
+          <input ref={zipCodeRef} className='border-2 pl-2' type="number" name="zipCode" id="zipCode" placeholder='Postleitzahl'/>  
       </div>
       <div className='mb-5'>
-        <input className='border-2 pl-2' type="text" name="street" id="street" placeholder='Straße'/>
-        <input className='border-2 pl-2' type="text" name="streetAdress" id="streetAdress" placeholder='Hausnummer'/>
+          <input ref={streetRef} className='border-2 pl-2' type="text" name="street" id="street" placeholder='Straße'/>
+          <input ref={streetAdressRef} className='border-2 pl-2' type="text" name="streetAdress" id="streetAdress" placeholder='Hausnummer'/>
       </div>
       <div>
-        <input className='border-2 pl-2 block w-full' type="email" name="email" id="email" placeholder='E-Mail'/>
-        <input className='border-2 pl-2 block w-full' type="password" name="password" id="password" placeholder='Passwort'/>
-        <input className='border-2 pl-2 mb-7 block w-full' type="password" name="confirm-password" id="confirm-password" placeholder='Passwort wiederholen'/>
+          <input ref={emailRef} className='mb-4 border-2 pl-2 block w-full' type="email" name="email" id="email" placeholder='E-Mail'/>
+          <input onChange={(e) => setPassword(e.target.value)} className='border-2 pl-2 block w-full' type="password" name="password" id="password" placeholder='Passwort'/>
+          {/* check for matching passwords */}
+          {password != confirmPassword && confirmPassword != undefined ? <p>Die Passwörter stimmen nicht überein.</p> : ""}
+          <p>{passwordErrorMessage}</p>
+          <input onClick={passwordValidationMessage} onChange={(e) => setConfirmPassword(e.target.value)} className='border-2 pl-2 mb-7 block w-full' type="password" name="confirmPassword" id="confirmPassword" placeholder='Passwort wiederholen'/>
       </div>
 
 
