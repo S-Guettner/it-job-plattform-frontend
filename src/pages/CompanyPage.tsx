2ec5aedd9ea920df7d397936a8e10bf6c45e56dd
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-05-06 16:38:13 +0200

    feat: added logout fetch to remove jwt cookie(needs to be moved to logout component)

--- src/pages/CompanyPage.tsx
+++ src/pages/CompanyPage.tsx
@@ -1,7 +1,30 @@
 import { FC } from 'react'
 import { Link } from 'react-router-dom'
+import useUserStore from '../global/zustand.ts'
+import LogoutButton from '../components/LogoutButton.tsx'
+import { useNavigate } from 'react-router-dom'
 
 const CompanyPage: FC = () => {
+
+    const userState = useUserStore()
+    console.log(userState.userLoggedInState)
+
+    const navigator = useNavigate()
+
+    const deleteJWT = async () => {
+        try {
+            const res = await fetch("http://localhost:9999/api/v1/company-logout" ,{
+                method: "POST",
+                credentials:"include",
+            })
+            if(res.ok){
+                navigator("/")
+            }
+
+        } catch (error) {
+            console.error(error)
+        }
+    }
 
 
     return (
@@ -11,8 +34,10 @@ const CompanyPage: FC = () => {
                     <p className='inline'>LOGO</p>
                     <div className='inline'>
 
-                        <Link className='mx-2' to={'/company-registration'}>Registrieren</Link>
-                        <Link className='mx-2' to={'/company-login'}>Einloggen</Link>
+                        <button onClick={deleteJWT}>delete jwt</button>
+                        {userState.userLoggedInState === false ? <Link className='mx-2' to={'/company-registration'}>Registrieren</Link>: ""}
+                        {userState.userLoggedInState === false ? <Link className='mx-2' to={'/company-login'}>Einloggen</Link> : <LogoutButton />}
+                        
 
                     </div>
                 </nav>
--- src/pages/DashboardCompany.tsx
+++ src/pages/DashboardCompany.tsx
@@ -1,10 +1,30 @@
 import { FC, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import useUserStore from '../global/zustand.ts'
-
+import { useNavigate } from 'react-router-dom'
 
 const DashboardCompany: FC = () => {
+
+  const navigator = useNavigate()
   const userState = useUserStore()
+
+
+  const deleteJWT = async () => {
+    try {
+      const res = await fetch("http://localhost:9999/api/v1/company-logout", {
+        method: "POST",
+        credentials: "include",
+      })
+      if (res.ok) {
+        navigator("/")
+      }
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+
   
   useEffect(() => {
     userState.userLoggedIn()
@@ -18,6 +38,7 @@ const DashboardCompany: FC = () => {
   return (
     <div>
       <h1>Yeahhhhh!!!!</h1>
+      <button onClick={deleteJWT}>logout</button>
       <p>{userState.userLoggedInState.toString()}</p>  
       <Link to={'/'}>Home</Link>  
     </div>
