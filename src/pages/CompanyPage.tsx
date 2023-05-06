e7efa9d0db16d48c73068c7ac403f7c4d4057ea2
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-05-06 19:23:55 +0200

    fix: removed logout logic from page to component

--- src/pages/CompanyPage.tsx
+++ src/pages/CompanyPage.tsx
@@ -2,29 +2,15 @@ import { FC } from 'react'
 import { Link } from 'react-router-dom'
 import useUserStore from '../global/zustand.ts'
 import LogoutButton from '../components/LogoutButton.tsx'
-import { useNavigate } from 'react-router-dom'
+
 
 const CompanyPage: FC = () => {
 
     const userState = useUserStore()
     console.log(userState.userLoggedInState)
 
-    const navigator = useNavigate()
 
-    const deleteJWT = async () => {
-        try {
-            const res = await fetch("http://localhost:9999/api/v1/company-logout" ,{
-                method: "POST",
-                credentials:"include",
-            })
-            if(res.ok){
-                navigator("/")
-            }
 
-        } catch (error) {
-            console.error(error)
-        }
-    }
 
 
     return (
@@ -34,7 +20,6 @@ const CompanyPage: FC = () => {
                     <p className='inline'>LOGO</p>
                     <div className='inline'>
 
-                        <button onClick={deleteJWT}>delete jwt</button>
                         {userState.userLoggedInState === false ? <Link className='mx-2' to={'/company-registration'}>Registrieren</Link>: ""}
                         {userState.userLoggedInState === false ? <Link className='mx-2' to={'/company-login'}>Einloggen</Link> : <LogoutButton />}
                         
